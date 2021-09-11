12f4ababca252e39014eb63d08d5248152b736c2
Steven Strybos <steven.strybos@pruvan.com>
2021-09-10 20:22:48 -0500

    Removed unused libs and tests

--- app/src/main/java/com/android/example/brzycki_formula/database/ExerciseDatabaseDao.kt
+++ app/src/main/java/com/android/example/brzycki_formula/database/ExerciseDatabaseDao.kt
@@ -4,7 +4,6 @@ import androidx.lifecycle.LiveData
 import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.Query
-import androidx.room.Update
 
 @Dao
 interface ExerciseDatabaseDao {
@@ -12,23 +11,13 @@ interface ExerciseDatabaseDao {
     @Insert
     suspend fun insertIteration(night: ExerciseIteration)
 
-    @Update
-    suspend fun updateIteration(night: ExerciseIteration)
-
-    @Query("SELECT * from exercise_iteration_table WHERE id = :key")
-    suspend fun getIteration(key: Long) : ExerciseIteration?
-
     @Query("DELETE FROM exercise_iteration_table")
     suspend fun clearIterations()
-
-    @Query("SELECT * FROM exercise_iteration_table ORDER BY id ASC")
-    fun getAllIterations(): List<ExerciseIteration>
 
     @Query("SELECT * FROM exercise_iteration_table WHERE exercise_name = :name ORDER BY exercise_date ASC")
     fun getIterationsByName(name: String): LiveData<List<ExerciseIteration>>
 
     @Query("SELECT exercise_name, max(max) as max FROM exercise_iteration_table GROUP BY exercise_name")
     fun getExercises(): LiveData<List<Exercise>>
-
 
 }
